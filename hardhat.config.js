56db1383e4e363820eef1a9fe4ba00c2e9e22773
Peter O'Leary <pete@timelight.com>
2022-04-16 12:10:02 -0700

    Change solidity version, add namedAccounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
 require('hardhat-circom');
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.6.7",
+  solidity: "0.6.11",
+  namedAccounts: {
+    deployer: 0,
+    tokenOwner: 1,
+  },
   circom: {
     inputBasePath: "./circuits",
     ptau: "pot15_final.ptau",
