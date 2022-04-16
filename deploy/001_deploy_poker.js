4094e7120ed193cc6b62640fb5d82756f26cfc01
Peter O'Leary <pete@timelight.com>
2022-04-16 12:10:58 -0700

    Add deploy

---
+++ deploy/001_deploy_poker.js
@@ -0,0 +1,34 @@
+// deploy/00_deploy_your_contract.js
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy } = deployments;
+    const namedAccounts = await getNamedAccounts()
+    console.log(`namedAccounts deployer=${JSON.stringify(namedAccounts)}`)
+    const { deployer } = namedAccounts;
+    console.log(`Verifier deployer=${deployer}`)
+    await deploy("Verifier", {
+      // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+      from: deployer,
+      // args: ["Hello"],
+      log: true,
+    });
+  
+    /*
+      // Getting a previously deployed contract
+      const YourContract = await ethers.getContract("YourContract", deployer);
+      await YourContract.setPurpose("Hello");
+      
+      //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+    */
+  };
+  module.exports.tags = ["Verifier"];
+  
+  /*
+  Tenderly verification
+  let verification = await tenderly.verify({
+    name: contractName,
+    address: contractAddress,
+    network: targetNetwork,
+  });
+  */
+  
